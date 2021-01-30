79f8df419e4c4500359134292cbedcd6cb3324fa
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2021-01-30 14:38:40 +0100

    Start new repository

--- src-msbuild/FredrikHr.XamarinPlayground.Notes.iOS/CreateDeveloperCertificate.ps1
+++
@@ -1,8 +0,0 @@
-$cert = New-SelfSignedCertificate -Type CodeSigningCert `
-    -CertStoreLocation Cert:\CurrentUser\My\ -KeyLength 2048 `
-    -Subject "CN=com.github.fredrikhr,OU=com.github.fredrikhr.xamarinplayground.notes,G=Fredrik Høisæther,SN=Rasch,L=Tromsø,C=NO,E=fredrik.rasch@gmail.com"
-$p12FileName = Join-Path $PSScriptRoot "$([System.IO.Path]::GetFileName($PSScriptRoot)).p12"
-$p12FileItem = $cert | Export-PfxCertificate -FilePath $p12FileName `
-    -Password $(Read-Host -AsSecureString -Prompt "PKCS#12 Password") `
-    -Verbose
-Write-Host "Code Signing Certificate exported to: " $p12FileItem.FullName
